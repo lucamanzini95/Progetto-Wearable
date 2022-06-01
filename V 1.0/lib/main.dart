40d54049f08fce4b7a3e521ae12e46181b0201cb
Luca Manzini <luca.manzini@studenti.unipd.it>
2022-06-01 15:06:11 +0200

    Prima versione del progetto

---
+++ V 1.0/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:multilevel_drawer/SecondScreen.dart';
+import 'package:multilevel_drawer/MultiLevelDrawer.dart';
+import 'package:multilevel_drawer/MyHomePage.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Progetto Biomedical',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
